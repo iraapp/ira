4f27bd8f55dbd38f826895f9328632fd11215df0
Ashutosh Chauhan <ashutoshc8101@gmail.com>
2022-07-01 17:05:15 +0530

    Introduced welcome view for the backend (#7)
    introduced welcome view for the backend

--- api/institute_app/urls.py
+++ api/institute_app/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from unicodedata import name
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
+    path('', views.welcome, name='welcome'),
     path('admin/', admin.site.urls),
     path("auth/", include("authentication.urls")),
     path('gate_pass/', include('gate_pass.urls')),
