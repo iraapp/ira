22082f9b5d6864060d7784c1cc4d72c74cd919df
Ashutosh Chauhan <ashutoshc8101@gmail.com>
2022-08-22 19:47:35 +0530

    Mess menu api implemented (#25)
    mess menu api implemented

--- api/mess/models.py
+++ api/mess/models.py
@@ -1,50 +1,47 @@
-from unicodedata import name
 from institute_app import settings
 from django.db import models
 
 class MenuItem(models.Model):
-  name = models.CharField(max_length=30, default='')
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=30, default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-  def __str__(self):
-        return self.name
+    def __str__(self):
+          return self.name
 
 class MenuSlot(models.Model):
-  name = models.CharField(max_length=15, default='')
-  items = models.ManyToManyField(MenuItem)
-  start_time = models.TimeField()
-  end_time = models.TimeField()
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=15, default='')
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-  def __str__(self):
-        name = ''
-        for item in self.items.all():
-          name += item.name + ' | '
-        return self.name + ' | ' + name
-
-class WeekDay(models.Model):
-  name = models.CharField(max_length=15, default='')
-  slots = models.ManyToManyField(MenuSlot)
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
-
-  def __str__(self):
+    def __str__(self):
         return self.name
 
+class WeekDay(models.Model):
+    name = models.CharField(max_length=15, default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+          return self.name
+
 class Mess(models.Model):
-  name = models.CharField(max_length=15, default='')
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=15, default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-  def __str__(self):
-    return self.name
+    def __str__(self):
+      return self.name
 
+class MessMenu(models.Model):
+    slot = models.ForeignKey(MenuSlot, on_delete=models.CASCADE)
+    items = models.ManyToManyField(MenuItem)
+    weekdays = models.ManyToManyField(WeekDay)
 
 # data base model for feedback
 # status is for whether the feedback is viewed by mess manager or not.
-
 class MessFeedback(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     body = models.TextField(editable = True, null = False, blank = True)
@@ -63,10 +60,10 @@ class MessMom(models.Model):
 
 # data base model for mess tender
 class MessTender(models.Model):
+    archieved = models.BooleanField(default = False)
     date = models.DateField(default=None, null=True)
     contractor  = models.CharField(max_length = 100)
     file = models.FileField(upload_to = 'tender/')
     title = models.CharField(max_length = 100)
     description = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now = True)
-    archived = models.BooleanField(default = False)
