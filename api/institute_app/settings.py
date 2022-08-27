50533f20532d9c41666beba9b8c3b3fdaa0b4e23
ashutoshc8101 <ashutoshc8101@gmail.com>
2022-08-27 16:21:59 +0530

    nits

--- api/institute_app/settings.py
+++ api/institute_app/settings.py
@@ -144,6 +144,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- server_configs/scripts/before_install.sh
+++ server_configs/scripts/before_install.sh
@@ -3,4 +3,6 @@
 
 sudo apt-get update
 sudo apt-get install -y python3-dev python3-pip apache2 libapache2-mod-wsgi-py3 python3.8-venv
+sudo rm -rf /etc/apache2/sites-enabled/000-default.conf
+sudo rm -rf /etc/apache2/sites-available/000-default.conf
 sudo rm -rf /var/www/backend
