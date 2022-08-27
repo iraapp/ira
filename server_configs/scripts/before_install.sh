4d9e4630bbc48d1607bca132ad07e897e6d6b799
ashutoshc8101 <ashutoshc8101@gmail.com>
2022-08-27 12:34:51 +0530

    nit

--- server_configs/scripts/before_install.sh
+++ server_configs/scripts/before_install.sh
@@ -2,6 +2,6 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get install -y python3 python-dev python3-pip ffmpeg supervisor apache2 libapache2-mod-wsgi
+sudo apt-get install -y python3 python-dev python3-pip ffmpeg supervisor apache2 libapache2-mod-wsgi-py3
 pip install --user --upgrade virtualenv
 sudo rm -rf /var/www/backend
