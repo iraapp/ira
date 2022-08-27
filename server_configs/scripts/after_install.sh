4c8afeb88175c783bd7570649f9e1b22aa7afb3b
Ashutosh Chauhan <ashutoshc8101@gmail.com>
2022-08-27 16:17:24 +0530

    Upgrade CD server to ubuntu 20.04 (#31)
    * add buildspec.yml for continous deployment
    
    * nit
    
    * nit
    
    * nit
    
    * nit
    
    * upgrade to ubuntu server 20.04

--- server_configs/scripts/after_install.sh
+++ server_configs/scripts/after_install.sh
@@ -2,8 +2,10 @@
 
 # Install libaries
 cd /var/www/backend
+cp .env.example .env
 python3 -m venv venv
 source venv/bin/activate
+pip install wheel
 pip install -r requirements.txt
 cd api
 python manage.py migrate
