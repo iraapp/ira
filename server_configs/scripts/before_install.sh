1340ea1d57533f80d569d792abc3712a47aafe29
Ashutosh Chauhan <ashutoshc8101@gmail.com>
2022-08-27 15:41:54 +0530

    Modifications to before_install.sh (#30)
    * add buildspec.yml for continous deployment
    
    * nit
    
    * nit
    
    * nit
    
    * nit

--- server_configs/scripts/before_install.sh
+++ server_configs/scripts/before_install.sh
@@ -2,6 +2,5 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get install -y python3 python3-pip python-dev python3-pip ffmpeg supervisor apache2 libapache2-mod-wsgi-py3
-pip install --user --upgrade virtualenv
+sudo apt-get install -y python3 python-dev python3-pip apache2 libapache2-mod-wsgi-py3 python3-pip
 sudo rm -rf /var/www/backend
