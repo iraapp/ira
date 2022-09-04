36fd60941640788fe995558cc3b57a52cf53cb45
Archit prasar <69184842+architprasar@users.noreply.github.com>
2022-09-04 21:28:47 +0530

    To Add Appointment models and views , Medical History models and views , updated the mess_type field to mess_meal field and fixed some false imports (#34)
    * AppointmentMedicalHistoryAndMinorFix
    
    * Update api/medical/views.py

--- api/medical/admin.py
+++ api/medical/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from medical.models import Doctor, Staff
+from medical.models import Doctor, Staff, Appointment, MedicalHistory
 
 # Register your models here.
 admin.site.register(Doctor)
 admin.site.register(Staff)
+admin.site.register(Appointment)
+admin.site.register(MedicalHistory)
--- api/medical/urls.py
+++ api/medical/urls.py
@@ -14,7 +14,7 @@ Note : All api url should start with medical/
                     3. mail - mail of doctor
                     4. age - age of doctor
                     5. start_time - timings of doctor
-                    6. joined_at - the date doctor joined 
+                    6. joined_at - the date doctor joined
                     7. details - details of doctor
                     8. phone - phone of doctor
                     9 .end_time - end time of doctor
@@ -30,24 +30,51 @@ Note : All api url should start with medical/
                     1. name - name of staff
                     2. specialization - specialization of staff
                     3. Phone - phone of staff
-     
+
      4. staff/<pk> -for  medical manager to get staff by id
           - get
           - update payload includes same as above
           - delete
-     
+
      5.  student/doctor/ - for student to get doctor list
           -get
 
      6.  student/doctor/<pk> - for student to get doctor by id
           -get
-     
+
      7.  student/staff/ - for student to get staff list
           -get
-     
+
      8. student/staff/<pk> - for student to get staff by id
           -get
 
+     9. appointment/ - for student to book appointment
+          -post payload required:
+               1. doctor - doctor id
+          
+          -get: returns all the appointment of student
+     
+     10. doctor/appointment/ - for doctor to view or update his appointments
+          -get: returns all the appointment of doctor
+          -post: payload required:
+               1. id - appointment id
+               2. accepted - true or false
+                  if accepted payload:
+                    1. date - date of appointment
+                    2. time - time of appointment
+                  if rejected payload:
+                    1. reason - reason for rejection
+
+     11. medicalhistory/ - to add medical history
+          -post: payload required:
+               1. doctor - doctor id
+               2. details - details of medical history
+               3. date - date of medical history
+               4. time - time of medical history
+               5. prescription - prescription of medical history
+               6. patient - patient id
+               7. inhouse - true or false
+          -get: returns all the medical history of student
 
 """
 
@@ -63,5 +90,11 @@ urlpatterns = [
     path('student/staff/', views.StudentStaffView.as_view(), name="student_staff"),
     path('student/staff/<int:pk>/', views.StudentStaffInstanceView.as_view(),
          name="student_staff_instance"),
+    path('appointment/', views.AppointmentView.as_view(), name="appointment"),
+    path('doctor/appointment/', views.DoctorAppointmentView.as_view(),
+         name="doctor_appointment"),
+    path('medicalhistory/', views.MedicalHistoryView.as_view(),
+         name="medical_history"),
+
 
 ]
