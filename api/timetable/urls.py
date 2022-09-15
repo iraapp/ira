77e8d8d044070cb2dd0f9eefea61166e98b8b191
architprasar <architprasar@gmail.com>
2022-09-15 20:41:39 +0530

    urls added

--- api/timetable/urls.py
+++ api/timetable/urls.py
@@ -3,8 +3,21 @@ from timetable.views import *
 
 
 urlpatterns = [
-    path('timetable', StudentTimetableView.as_view(), name="Timetable"),
-    path("slot", StudentSlotView.as_view(), name="Slot"),
-    path("day", StudentDayView.as_view(), name="Day"),
-    path("course", StudentCourseView.as_view(), name="Course"),
+    path('student/timetable', StudentTimetableView.as_view(), name="Timetable"),
+    path("student/slot", StudentSlotView.as_view(), name="Slot"),
+    path("student/day", StudentDayView.as_view(), name="Day"),
+    path("student/course", StudentCourseView.as_view(), name="Course"),
+    path("student/timetable/<int:pk>",
+         StudentTimetableInstanceView.as_view(), name="TimetableInstance"),
+    path("student/slot/<int:pk>",
+         StudentSlotInstanceView.as_view(), name="SlotInstance"),
+    path("student/day/<int:pk>",
+         StudentDayInstanceView.as_view(), name="DayInstance"),
+    path("student/course/<int:pk>",
+         StudentCourseInstanceView.as_view(), name="CourseInstance"),
+    path("timetable", TimeTableView.as_view(), name="TimetableSlot"),
+    path("slot", SlotView.as_view(), name="Slot"),
+    path("day", DayView.as_view(), name="Day"),
+    path("course", CourseView.as_view(), name="Course"),
+
 ]
