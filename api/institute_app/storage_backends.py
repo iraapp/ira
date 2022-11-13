76d4cd452f1681b33fe5687581bf228d3404fd3c
Ashutosh Chauhan <ashutoshc8101@gmail.com>
2022-11-13 21:27:51 +0530

    Introduce S3 to store static and media files. (#71)
    * Revert "Revert "Introduces history manager screen for medical manager" (#57)"
    
    This reverts commit 711aaea60d9a7b7c30397b426916d09668e90359.
    
    * merge changes from beta-release
    
    * Introduce amazon s3 to store static and media files

---
+++ api/institute_app/storage_backends.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class StaticStorage(S3Boto3Storage):
+    location = 'static'
+
+
+class PublicMediaStorage(S3Boto3Storage):
+    location = 'media'
